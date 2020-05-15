3cb2b6ee2b04f184d809ffd82f4c62672421754c
sjt <1269879928@qq.com>
2020-05-15 18:00:07 +0800

    videos

--- src/components/videos/create.vue
+++ src/components/videos/create.vue
@@ -10,33 +10,31 @@
   <div class="app-form frame-page basic-form-vue">
     <div class="h-panel">
       <div class="h-panel-bar">
-        <span class="h-panel-title">添加课程</span>
+        <span class="h-panel-title">添加视频</span>
       </div>
       <div class="h-panel-body">
         <p>
          <Button class="h-btn h-btn-primary" icon="icon-arrow-left" @click="back()">返回</Button>
        </p>
        <Form  mode="block" ref="form" :validOnChange="true" :showErrorTip="true" :labelWidth="110" :rules="validRules" :model="courses">
-          <FormItem label="分类" prop="category_id">
-            <template v-slot:label>分类</template>
+          <FormItem label="课程" prop="category_id">
+            <template v-slot:label>课程</template>
+            <Select v-model="courses.category_id" :datas="categories" keyName="id" titleName="name"></Select>
+          </FormItem>
+          <FormItem label="章节" prop="category_id">
+            <template v-slot:label>章节</template>
             <Select v-model="courses.category_id" :datas="categories" keyName="id" titleName="name"></Select>
           </FormItem>
           <FormItem label="名称" prop="title">
             <template v-slot:label>标题</template>
             <input type="text" v-model="courses.title" />
           </FormItem>
-          <FormItem label="名称" prop="price">
-            <template v-slot:label>售价</template>
-             <div class="h-input-group" v-width="300">
-              <input  type="number" v-model="courses.price" />
-              <span class="h-input-addon">元</span>
-            </div>
-            <span class="h-tag h-tag-yellow">0元表示免费</span>
-          </FormItem>
-          <FormItem label="封面" prop="thumb">
-            <template v-slot:label>封面</template>
-             <!-- <image-upload v-model="courses.thumb" name="封面"></image-upload> -->
-             <upload-image v-model="courses.thumb" action="/backend/v1/upload/image"></upload-image>
+          <FormItem label="试看" prop="status">
+            <template v-slot:label>试看</template>
+            <h-switch v-model="courses.status">
+              <span slot="open" class="h-icon-check"></span>
+              <span slot="close" class="h-icon-close"></span>
+            </h-switch>
           </FormItem>
           <FormItem label="显示" prop="status">
             <template v-slot:label>显示</template>
@@ -44,17 +42,6 @@
               <span slot="open" class="h-icon-check"></span>
               <span slot="close" class="h-icon-close"></span>
             </h-switch>
-          </FormItem>
-          <FormItem label="推荐" prop="is_rec">
-            <template v-slot:label>推荐</template>
-            <h-switch v-model="courses.is_rec">
-              <span slot="open" class="h-icon-check"></span>
-              <span slot="close" class="h-icon-close"></span>
-            </h-switch>
-          </FormItem>
-          <FormItem label="上架时间" prop="published_at">
-            <template v-slot:label>上架时间</template>
-            <DatePicker v-model="courses.published_at" v-width="300" type="datetime"></DatePicker>
           </FormItem>
           <FormItem label="seo关键词" prop="seo_keywords">
             <template v-slot:label>seo关键词</template>
@@ -79,11 +66,9 @@
 <script>
 import Courses from 'model/Courses';
 import tinymce from '../common/tinymce';
-import UploadImage from '../common/upload-image';
 export default {
   components: {
-    Tinymce: tinymce,
-    UploadImage
+    Tinymce: tinymce
   },
   data() {
     return {
@@ -129,7 +114,7 @@ export default {
       });
     },
     back() {
-      this.$router.push({ name: 'Course' });
+      this.$router.back();
     },
     submit() {
       let validResult = this.$refs.form.valid();
--- src/js/config/menu-config.js
+++ src/js/config/menu-config.js
@@ -1,9 +1,23 @@
 const fullMenus = [
   {
-    title: '首页',
+    title: '概况',
     key: 'Home',
     icon: 'icon-monitor',
     count: 0
+  },
+  {
+    title: '用户',
+    key: 'Users',
+    icon: 'icon-head',
+    children: [
+    ]
+  },
+  {
+    title: '订单',
+    key: 'Orders',
+    icon: 'icon-paper',
+    children: [
+    ]
   },
   {
     title: '视频',
@@ -25,7 +39,7 @@ const fullMenus = [
     ]
   },
   {
-    title: '系统',
+    title: '设置',
     key: 'System',
     icon: 'icon-cog',
     children: [
@@ -41,109 +55,6 @@ const fullMenus = [
         title: '管理员',
         key: 'Administrator'
       }
-    ]
-  },
-  {
-    title: 'Icons',
-    key: 'Icons',
-    icon: 'icon-heart'
-  },
-  {
-    title: '列表应用',
-    key: 'tablelist',
-    icon: 'icon-grid-2',
-    children: [
-      {
-        title: '基础表格',
-        key: 'TableBasic'
-      },
-      {
-        title: '查询列表',
-        key: 'TableSearch'
-      }
-    ]
-  },
-  {
-    title: '表单应用',
-    key: 'form-folder',
-    icon: 'icon-paper',
-    children: [
-      {
-        title: '基础表单',
-        key: 'Form'
-      },
-      {
-        title: '表单详情',
-        key: 'FormDetail'
-      }
-    ]
-  },
-  {
-    title: '模糊匹配',
-    key: 'AutoComplete-folder',
-    icon: 'icon-disc',
-    children: [
-      {
-        title: '模糊搜索',
-        key: 'Autocomplete1'
-      },
-      {
-        title: '场景应用',
-        key: 'Autocomplete2'
-      },
-      {
-        title: '复杂场景',
-        key: 'Autocomplete3'
-      }
-    ]
-  },
-  {
-    title: '扩展组件',
-    key: 'Advance-folder',
-    icon: 'icon-bar-graph-2',
-    children: [
-      {
-        title: '图表',
-        key: 'Chart'
-      },
-      {
-        title: '富文本编辑器',
-        key: 'RicktextEditor'
-      },
-      {
-        title: '代码编辑器',
-        key: 'CodeEditor'
-      },
-      {
-        title: 'Markdown编辑器',
-        key: 'MarkdownEditor'
-      }, {
-        title: '百度地图',
-        key: 'BaiduMap'
-      }
-    ]
-  },
-  {
-    title: '系统设置',
-    key: 'SysSetting',
-    icon: 'icon-cog',
-    children: [
-      {
-        title: '个人中心',
-        key: 'AccountBasic'
-      },
-      {
-        title: '安全设置',
-        key: 'SecuritySetting'
-      },
-      {
-        title: '权限设置',
-        key: 'Authorization'
-      }
-      // {
-      //   title: '用户管理',
-      //   key: 'Users'
-      // }
     ]
   }
 ];
